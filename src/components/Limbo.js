c43d75b5e217dd5b207db4274bd0a363aa832374
mannybennett <mannybennett99@gmail.com>
2023-07-18 17:58:28 -0400

    Comment section functionality

--- src/components/Limbo.js
+++ src/components/Limbo.js
@@ -2,14 +2,41 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from "@auth0/auth0-react";
 import ReactAudioPlayer from 'react-audio-player';
-import { Card, Button, Box, CardContent, CardMedia, Typography, CardActions, Collapse, IconButton, styled, Checkbox } from '@mui/material';
+import { useMediaQuery, Card, Button, Box, CardContent, CardMedia, Typography, CardActions, Collapse, IconButton, styled, Checkbox, Divider, Modal, TextField, Avatar } from '@mui/material';
 import InsertCommentOutlinedIcon from '@mui/icons-material/InsertCommentOutlined';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const Limbo = (props) => {
   const { user:auth0User } = useAuth0();
   const [expandedId, setExpandedId] = useState(null);
   const [clickedButton, setClickedButton] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('')
   // const [allVotes, setAllVotes] = useState([]);
+  const [open, setOpen] = useState(false);
+  
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const mobileView = useMediaQuery('(max-width: 600px)');
+
+  const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: mobileView ? '50%' : '49.7%',
+    transform: 'translate(-50%, -50%)',
+    width: mobileView ? '90%' : '50%',
+    maxWidth: '900px',
+    bgcolor: '#1f1f1f',
+    borderRadius: 1,
+    boxShadow: 24,
+    p: 2,
+    textAlign: "center"
+  };
+
+  const inputComment = (e) => {
+    setNewComment(e.target.value)
+  }
 
   const handleExpandClick = (id) => {
     setExpandedId(id === expandedId ? null : id);
@@ -41,23 +68,21 @@ const Limbo = (props) => {
     await axios.get('/api/audioFiles/')
     .then(res => props.getFiles(res.data))
   }
-
-  // const setButtons = (votes) => {
-  //   const userVotes = votes.filter(vote => vote.user_id === props.user.id);
-  //   console.log('Votes:', userVotes);
-  
-  //   const initialClickedButton = userVotes.reduce((acc, vote) => {
-  //     acc[vote.audio_file_id] = vote.vote;
-  //     return acc;
-  //   }, {});
-  
-  //   setClickedButton(initialClickedButton);
-  // };
   
   const getAllVotes = async () => {
     try {
       const response = await axios.get('/api/limbo/');
       return response.data;
+    } catch (error) {
+      console.log('Error fetching votes:', error);
+      throw error;
+    }
+  };
+  
+  const getAllComments = async () => {
+    try {
+      const response = await axios.get('/api/limbo/comments');
+      return setComments(response.data)
     } catch (error) {
       console.log('Error fetching votes:', error);
       throw error;
@@ -77,9 +102,43 @@ const Limbo = (props) => {
     }
   };
 
+  const postComment = async (comment, userId, userName, audioFileId, profilePicture) => {
+    try {
+      await axios.post('/api/limbo/comments', {
+        comment: comment,
+        user_id: userId,
+        user_name: userName,
+        audio_file_id: audioFileId,
+        profile_picture: profilePicture
+      });
+      getAllComments()
+      console.log('Comment posted successfully');
+    } catch (error) {
+      console.error('Error posting comment:', error);
+    }
+  };
+
+  const sendComment = async (e) => {
+    e.preventDefault()
+    await postComment(newComment, props.user.id, props.user.user_name, expandedId, props.user.profile_picture)
+    handleClose()
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    getAudioFiles();
+  }, []);
+
+  useEffect(() => {
+    getAllComments()
+  }, []);
+
   useEffect(() => {
     // NOT WORKING ON FIRST RENDER
-    const fetchData = async () => {
+    const fetchVotes = async () => {
       try {
         const votes = await getAllVotes();
         const userVotes = votes.filter(vote => vote.user_id === props.user.id);
@@ -94,15 +153,12 @@ const Limbo = (props) => {
       }
     };
 
-    fetchData();
+    fetchVotes();
   }, []);
 
-  useEffect(() => {
-    getUser();
-    getAudioFiles();
-  }, []);
 
-console.log("User", props.user)
+// console.log("User", props.user)
+console.log("Comments", comments)
 // console.log("Files", props.audioFiles)
 
   return (
@@ -130,8 +186,9 @@ console.log("User", props.user)
                         sx={{  }}
                         variant="p"
                         color="#919191"
-                        component="div">
-                          {file.user_name}
+                        component="div"
+                      >
+                        {file.user_name}
                       </Typography>
                     </Box>
                   </Box>
@@ -178,13 +235,80 @@ console.log("User", props.user)
                 </ExpandMore>
               </CardActions>
               <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+                <Divider sx={{ backgroundColor: '#919191' }} variant="middle" />
                 <CardContent>
-                  <Typography color="#e8e8e8" component="div" variant="h5">Comments</Typography>
-                  <Typography color="#e8e8e8" component="div" variant="subtitle1">
-                    This audio clip is very interesting, I appreciate the way it was mixed but am not sure about your sound selection. Keep up the good work.
-                  </Typography>
+                  <Typography marginBottom={1.2} color="#e8e8e8" component="div" variant="h5">Comments</Typography>
+                  {comments.length > 0 &&
+                    comments.map((comment, idx) => {
+                      if (expandedId === comment.audio_file_id) {
+                        return (
+                          <Box sx={{ marginBottom: 1 }} key={idx}>
+                            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                              <Avatar sx={{ width: 24, height: 24, marginRight: 1 }} alt='' src={comment.profile_picture} />
+                              <Typography                            
+                                variant="p"
+                                color="#919191"
+                                component="div"
+                              >
+                                {comment.user_name}
+                              </Typography>
+                            </Box>                    
+                            <Typography sx={{ paddingLeft: '32px' }} color="#e8e8e8" component="div" variant="subtitle1">
+                              {comment.comment}
+                            </Typography>
+                            {/* <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                              <Divider sx={{ backgroundColor: '#919191', margin: '12px 0', width: '60%' }} />                  
+                            </Box> */}
+                          </Box> 
+                        )
+                      }
+                  })}                  
                 </CardContent>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                  <IconButton>
+                    <AddBoxIcon onClick={handleOpen} sx={{ fill: '#e8e8e8' }} fontSize='large' />
+                  </IconButton>
+                </Box>                
               </Collapse>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                slotProps={{
+                  backdrop: {
+                    sx: {
+                      opacity: '0.3 !important',
+                      position: 'fixed !important',
+                      zIndex: -1,
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0
+                    }
+                  }
+                }}
+              >
+                <Box sx={modalStyle}>
+                  <form onSubmit={sendComment} className='commentForm'>
+                    <TextField
+                      onChange={inputComment}
+                      sx={{
+                        marginBottom: 2,
+                        background: 'white',
+                        borderRadius: 1,
+                      }}
+                      inputProps={{ maxLength: 80 }}
+                      fullWidth={true}
+                      color='primary'
+                      label="Comment"
+                      variant="filled"
+                      multiline
+                      required
+                    />
+                    <Button sx={{ width: '20%' }} type='submit' variant='contained'>POST</Button>
+                  </form>                  
+                </Box>
+              </Modal>
             </Card>
           )
         }
