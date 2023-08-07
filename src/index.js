21b80773feec0b26d01918d5312ccf8ea29f5204
mannybennett <mannybennett99@gmail.com>
2023-08-07 13:43:05 -0400

    url

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: "http://localhost:3000/profilecreation"
+        redirect_uri: "https://beatlimbo-frontend.onrender.com/profilecreation"
       }}
       useRefreshTokens={true}
       cacheLocation="localstorage"
