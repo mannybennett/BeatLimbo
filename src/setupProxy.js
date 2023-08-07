005081ca92320dfd883c1cd4ddd33c8826bae883
mannybennett <mannybennett99@gmail.com>
2023-08-07 13:08:57 -0400

    Proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:4000',
+      target: 'https://beatlimbo-backend.onrender.com',
       changeOrigin: true,
     })
   );
